dc96676b7a60e6107fa9483dcea46619b79916a5
wachcio <wachcio@poczta.fm>
2020-10-27 00:42:47 +0100

    Add css modules to returnButton

--- log_app/src/components/ReturnButton/ReturnButton.jsx
+++ log_app/src/components/ReturnButton/ReturnButton.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styles from './ReturnButton.module.scss';
 
 const ReturnButton = () => {
-  const history = useHistory();
-  const handleOnClick = () => history.goBack();
-  return (
-    <button className="returnButton" onClick={handleOnClick}>
-      wstecz
-    </button>
-  );
+    const history = useHistory();
+    const handleOnClick = () => history.goBack();
+    return (
+        <button className={styles.returnButton} onClick={handleOnClick}>
+            wstecz
+        </button>
+    );
 };
 
 export default ReturnButton;
