e055c4b4902ee3b55d9ec787da6839086532cb06
wachcio <wachcio@poczta.fm>
2020-10-26 22:31:02 +0100

    change App to Root

---
+++ log_app/src/views/Root/Root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HomePage from '../../pages/HomePage';
+import './index.scss';
+
+const Root = () => {
+    return <div className="Root">{<HomePage />}</div>;
+};
+
+export default Root;
