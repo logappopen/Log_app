82f9243f2383724a68db6f1a735f515ee50337a9
wachcio <wachcio@poczta.fm>
2020-10-26 23:10:13 +0100

    change name Components to Main

--- log_app/src/views/Home/Home.jsx
+++ log_app/src/views/Home/Home.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Header from "../layouts/Header";
-import Components from "../layouts/Components";
+import Header from '../../components/Header/Header';
+import Main from '../Main/Main';
 
 class HomePage extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <div className="container__header">{<Header />}</div>
-          <div className="container__section">{<Components />}</div>
-          <div className="containe__footer"></div>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <div className="container__header">{<Header />}</div>
+                    <div className="container__section">{<Main />}</div>
+                    <div className="containe__footer"></div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default HomePage;
--- log_app/src/views/Main/Main.jsx
+++ log_app/src/views/Main/Main.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import StartPage from "../pages/StartPage";
-import ContactPage from "../pages/ContactPage";
-import HelpPage from "../pages/HelpPage";
-import LoginPage from "../pages/LoginPage";
-import RegistrationPage from "../pages/RegistrationPage";
-import FreeStartPage from "./FreeStartPage";
-import LostPasswordPage from "../pages/LostPasswordPage";
-import ErrorPage from "../pages/ErrorPage";
+import StartPage from '../../pages/StartPage';
+import ContactPage from '../../pages/ContactPage';
+import HelpPage from '../../pages/HelpPage';
+import LoginPage from '../../pages/LoginPage';
+import RegistrationPage from '../../pages/RegistrationPage';
+import FreeStartPage from '../../layouts/FreeStartPage';
+import LostPasswordPage from '../../pages/LostPasswordPage';
+import ErrorPage from '../../pages/ErrorPage';
 
-const Components = (props) => {
-  return (
-    <Switch>
-      <Route path="/" exact component={StartPage} />
-      <Route path="/contact" component={ContactPage} />
-      <Route path="/help" component={HelpPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/registration" component={RegistrationPage} />
-      <Route path="/free_start" component={FreeStartPage} />
-      <Route path="/lost_password" component={LostPasswordPage} />
-      <Route path="/empty" component={ErrorPage} />
-    </Switch>
-  );
+const Main = (props) => {
+    return (
+        <Switch>
+            <Route path="/" exact component={StartPage} />
+            <Route path="/contact" component={ContactPage} />
+            <Route path="/help" component={HelpPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/registration" component={RegistrationPage} />
+            <Route path="/free_start" component={FreeStartPage} />
+            <Route path="/lost_password" component={LostPasswordPage} />
+            <Route path="/empty" component={ErrorPage} />
+        </Switch>
+    );
 };
 
-export default Components;
+export default Main;
