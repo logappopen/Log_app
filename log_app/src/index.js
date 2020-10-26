3b775f06a092f621c508d3ad942f83bf417ba0c7
wachcio <wachcio@poczta.fm>
2020-10-26 23:12:31 +0100

    add index.scss

--- log_app/src/index.js
+++ log_app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import Root from './views/Root/Root';
 import * as serviceWorker from './serviceWorker';
 
--- log_app/src/views/Root/Root.js
+++ log_app/src/views/Root/Root.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from '../Home/Home';
-import './index.scss';
 
 const Root = () => {
     return <div className="Root">{<Home />}</div>;
