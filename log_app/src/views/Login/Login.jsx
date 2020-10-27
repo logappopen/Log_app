377797a89f76eb971e72ddad08326e9c64c074b2
wachcio <wachcio@poczta.fm>
2020-10-27 12:01:41 +0100

    Fix email regex

--- log_app/src/views/Login/Login.jsx
+++ log_app/src/views/Login/Login.jsx
@@ -7,7 +7,7 @@ import ReturnButton from '../../components/ReturnButton/ReturnButton';
 const LoginPage = () => {
     const handleOnPushEmail = (e) => {
         // don't remember from where i copied this code, but this works.
-        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (re.test(e.target.value)) {
             // this is a valid email address
