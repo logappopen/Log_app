fb1f9a5b2ae2e97a92c6c024ebf492d46889d093
wachcio <wachcio@poczta.fm>
2020-10-30 01:42:58 +0100

    Use useState hook

--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -1,112 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 import styles from './Login.module.scss';
 
 import ReturnButton from '../../components/ReturnButton/ReturnButton';
 
-class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            isValidEmail: false,
-            password: '',
-        };
-    }
-    render() {
-        const handleOnPushEmail = (e) => {
-            // don't remember from where i copied this code, but this works.
-            let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [isValidEmail, setIsValidEmail] = useState(false);
+    const [password, setPassword] = useState('');
 
-            if (re.test(e.target.value)) {
-                // this is a valid email address
-                this.setState({ isValidEmail: true });
-            } else {
-                // invalid email, maybe show an error to the user.
-                this.setState({ isValidEmail: false });
-            }
-            this.setState({ email: e.target.value });
-        };
-        const sendCredentials = () => {
-            return axios
-                .post(process.env.REACT_APP_API_LOGIN_URL, {
-                    username: this.state.email,
-                    password: this.state.password,
-                })
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        };
+    const handleOnPushEmail = (e) => {
+        // don't remember from where i copied this code, but this works.
+        let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        const handleOnPushPassword = (e) => {
-            this.setState({ password: e.target.value });
-        };
+        if (re.test(e.target.value)) {
+            // this is a valid email address
+            setIsValidEmail(true);
+        } else {
+            // invalid email, maybe show an error to the user.
+            setIsValidEmail(false);
+        }
+        setEmail(e.target.value);
+    };
+    const sendCredentials = () => {
+        return axios
+            .post(process.env.REACT_APP_API_LOGIN_URL, {
+                username: email,
+                password: password,
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
 
-        const handleOnClickLogin = (e) => {
-            e.preventDefault();
+    const handleOnPushPassword = (e) => {
+        setPassword(e.target.value);
+    };
 
-            if (this.state.isValidEmail) {
-                console.log('Wpisano poprawny email');
-                sendCredentials();
-            } else {
-                console.log('Wpisano błędny email');
-            }
-            console.log(this.state);
-        };
+    const handleOnClickLogin = (e) => {
+        e.preventDefault();
 
-        return (
-            <div className={styles['section-login']}>
-                <form className={styles['login-form']} action="submit">
-                    <label
-                        className={styles['login-form__label']}
-                        htmlFor="login"
-                    >
-                        Adres email
-                    </label>
-                    <input
-                        value={this.state.email}
-                        type="email"
-                        name="login"
-                        onChange={handleOnPushEmail}
-                        className={styles['login-form__input']}
-                    />
-                    <label
-                        className={styles['login-form__label']}
-                        htmlFor="password"
-                    >
-                        Hasło
-                    </label>
-                    <input
-                        value={this.state.password}
-                        type="password"
-                        name="password"
-                        onChange={handleOnPushPassword}
-                        className={styles['login-form__input']}
-                    />
-                    <button
-                        className={styles['login-form__button']}
-                        onClick={handleOnClickLogin}
-                    >
-                        Logowanie
-                    </button>
-                </form>
-                <nav className={styles['section-login__nav']}>
-                    <ul>
-                        <NavLink to="/lost_password">
-                            Zapomniałem hasła...
-                        </NavLink>
-                    </ul>
-                </nav>
-                <div className={styles['section-login__return-button']}>
-                    <ReturnButton />
-                </div>
+        if (isValidEmail) {
+            console.log('Wpisano poprawny email');
+            sendCredentials();
+        } else {
+            console.log('Wpisano błędny email');
+        }
+        console.log(email, password, isValidEmail);
+    };
+
+    return (
+        <div className={styles['section-login']}>
+            <form className={styles['login-form']} action="submit">
+                <label className={styles['login-form__label']} htmlFor="login">
+                    Adres email
+                </label>
+                <input
+                    value={email}
+                    type="email"
+                    name="login"
+                    onChange={handleOnPushEmail}
+                    className={styles['login-form__input']}
+                />
+                <label
+                    className={styles['login-form__label']}
+                    htmlFor="password"
+                >
+                    Hasło
+                </label>
+                <input
+                    value={password}
+                    type="password"
+                    name="password"
+                    onChange={handleOnPushPassword}
+                    className={styles['login-form__input']}
+                />
+                <button
+                    className={styles['login-form__button']}
+                    onClick={handleOnClickLogin}
+                >
+                    Logowanie
+                </button>
+            </form>
+            <nav className={styles['section-login__nav']}>
+                <ul>
+                    <NavLink to="/lost_password">Zapomniałem hasła...</NavLink>
+                </ul>
+            </nav>
+            <div className={styles['section-login__return-button']}>
+                <ReturnButton />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default LoginPage;
