e687290d0b33377e692bfc344d97e46472ed35d8
DawidWebb <tomasz.matras@developerweb.pl>
2020-10-31 09:30:25 +0100

    fix: remove BEN from Contact, Help and FreeStart pages

--- frontend/src/views/Contact/Contact.jsx
+++ frontend/src/views/Contact/Contact.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './Contact.module.scss';
+import React from "react";
+import styles from "./Contact.module.scss";
 
 const ContactPage = (props) => {
-    return (
-        <div className={styles['section-contact']}>
-            <h1>Contact_Page</h1>
-        </div>
-    );
+  return (
+    <div className={styles["sectionContact"]}>
+      <h1>Contact_Page</h1>
+    </div>
+  );
 };
 
 export default ContactPage;
