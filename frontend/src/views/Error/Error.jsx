5cca81fef61fc6d822263a7835419e8139e1a6ff
wachcio <wachcio@poczta.fm>
2020-10-31 23:56:20 +0100

    error page finish

--- frontend/src/views/Error/Error.jsx
+++ frontend/src/views/Error/Error.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-// import styles from "./Error.module.scss"
+import React from 'react'
+import styles from './Error.module.scss'
+import img404 from '../../assets/images/404.png'
 
-const ErrorPage = (props) => {
-    return <div>Nie ma takiej strony</div>;
-};
+const ErrorPage = () => {
+    return (
+        <div className={styles.wrapper}>
+            <img src={img404} alt="" />
+            <div className={styles.info}>
+                <h1>404</h1>
+                <p>Niestety nie znaleziono strony.</p>
+            </div>
+        </div>
+    )
+}
 
-export default ErrorPage;
+export default ErrorPage
