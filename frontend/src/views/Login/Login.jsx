6652e466a54a79df2f94cefebdde0ba4cb5e1f48
DawidWebb <tomasz.matras@developerweb.pl>
2020-10-31 09:43:36 +0100

    fix: remove BEM from Start, Login, LostPassword pages

--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -1,70 +1,64 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './Login.module.scss';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Login.module.scss";
 
-import ReturnButton from '../../components/ReturnButton/ReturnButton';
+import ReturnButton from "../../components/ReturnButton/ReturnButton";
 
 const LoginPage = () => {
-    const handleOnPushEmail = (e) => {
-        // don't remember from where i copied this code, but this works.
-        let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const handleOnPushEmail = (e) => {
+    // don't remember from where i copied this code, but this works.
+    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        if (re.test(e.target.value)) {
-            // this is a valid email address
-            console.log(e.target.value, 'Email poprawny');
-        } else {
-            // invalid email, maybe show an error to the user.
-            console.log(e.target.value, 'Błędny email');
-        }
-    };
+    if (re.test(e.target.value)) {
+      // this is a valid email address
+      console.log(e.target.value, "Email poprawny");
+    } else {
+      // invalid email, maybe show an error to the user.
+      console.log(e.target.value, "Błędny email");
+    }
+  };
 
-    const handleOnPushPassword = (e) => {};
+  const handleOnPushPassword = (e) => {};
 
-    const handleOnClickLogin = () => {
-        console.log('klik');
-    };
+  const handleOnClickLogin = () => {
+    console.log("klik");
+  };
 
-    return (
-        <div className={styles['section-login']}>
-            <form className={styles['login-form']} action="submit">
-                <label className={styles['login-form__label']} htmlFor="login">
-                    Adres email
-                </label>
-                <input
-                    type="email"
-                    name="login"
-                    onChange={handleOnPushEmail}
-                    className={styles['login-form__input']}
-                />
-                <label
-                    className={styles['login-form__label']}
-                    htmlFor="password"
-                >
-                    Hasło
-                </label>
-                <input
-                    type="password"
-                    name="password"
-                    onChange={handleOnPushPassword}
-                    className={styles['login-form__input']}
-                />
-                <button
-                    className={styles['login-form__button']}
-                    onClick={handleOnClickLogin}
-                >
-                    Logowanie
-                </button>
-            </form>
-            <nav className={styles['section-login__nav']}>
-                <ul>
-                    <NavLink to="/lost_password">Zapomniałem hasła...</NavLink>
-                </ul>
-            </nav>
-            <div className={styles['section-login__return-button']}>
-                <ReturnButton />
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles["sectionLogin"]}>
+      <form className={styles["form"]} action="submit">
+        <label className={styles["label"]} htmlFor="login">
+          Adres email
+        </label>
+        <input
+          type="email"
+          name="login"
+          onChange={handleOnPushEmail}
+          className={styles["input"]}
+        />
+        <label className={styles["label"]} htmlFor="password">
+          Hasło
+        </label>
+        <input
+          type="password"
+          name="password"
+          onChange={handleOnPushPassword}
+          className={styles["input"]}
+        />
+        <button className={styles["button"]} onClick={handleOnClickLogin}>
+          Logowanie
+        </button>
+      </form>
+      <nav className={styles["nav"]}>
+        <ul>
+          <NavLink to="/lost_password">Zapomniałem hasła...</NavLink>
+        </ul>
+      </nav>
+      <div className={styles["returnButton"]}>
+        <ReturnButton />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
