79fb51de9f95cadb21895d5b836e50b6f65a1663
wachcio <wachcio@poczta.fm>
2020-11-04 14:45:53 +0100

    Show login info

--- frontend/src/components/LoginInfo/LoginInfo.jsx
+++ frontend/src/components/LoginInfo/LoginInfo.jsx
@@ -1,20 +1,24 @@
-/* eslint-disable react/no-array-index-key */
 import React, { useContext } from 'react';
+
+import { NavLink } from 'react-router-dom';
 
 import { StoreContext } from '../../store/StoreProvider';
 import styles from './LoginInfo.module.scss';
 
 const LoginInfo = () => {
-    const { userData, setUserData } = useContext(StoreContext);
+    const { userData } = useContext(StoreContext);
 
-    const Info = () => {
-        if (userData.isLogged) {
-            return `Witaj ${userData.username}`;
-        }
-
-        return `Zaloguj się`;
-    };
-    return <div className={styles.loginInfo}>{Info()}</div>;
+    return (
+        <div className={styles.loginInfo}>
+            {!userData.isLogged ? (
+                <NavLink to="/login" className={styles.link}>
+                    Zaloguj się
+                </NavLink>
+            ) : (
+                `Witaj ${userData.username}`
+            )}
+        </div>
+    );
 };
 
 export default LoginInfo;
