14dcfe6c584c1d20962308e378557c068c355e55
wachcio <wachcio@poczta.fm>
2020-11-04 01:08:35 +0100

    change spinner type

--- frontend/src/components/Spinner/Spinner.jsx
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -9,7 +9,7 @@ const Spinner = () => {
         <>
             {promiseInProgress && (
                 <div className={styles.loader}>
-                    <Loader type="Grid" color="#00BFFF" height={100} width={100} />
+                    <Loader type="ThreeDots" color="#00BFFF" height={100} width={100} />
                 </div>
             )}
         </>
