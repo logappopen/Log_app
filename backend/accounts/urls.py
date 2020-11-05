ce267ef5587952c323a2e738174df03789cd6b32
KamilWilczek <kamilwilczek@wp.pl>
2020-11-05 23:43:48 +0100

    backend lost password

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .api import RegisterAPI, LoginAPI, UserAPI
+from .api import RegisterAPI, LoginAPI, UserAPI, RequestPasswordResetEmail, PasswordTokenCheckAPI, SetNewPasswordAPIView
 from knox import views as knox_views
 
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path('api/auth/login', LoginAPI.as_view()),
     path('api/auth/user', UserAPI.as_view()),
     path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('api/request-reset-email', RequestPasswordResetEmail.as_view()),
+    path('api/password-reset/<uidb64>/<token>', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('api/password-reset-complete', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
 ]
